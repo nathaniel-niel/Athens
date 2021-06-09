1d66b541e64faf7f42df70b7f0b56aa29dcf62da
LowSpecx <mauricetin.916@gmail.com>
2021-06-09 10:48:29 +0700

    Timer utilities and workout model

---
+++ Mini2 Athens/Models/SuggestedWorkout.swift
@@ -0,0 +1,14 @@
+//
+//  SuggestedWorkout.swift
+//  Mini2 Athens
+//
+//  Created by Maurice Tin on 08/06/21.
+//
+
+import Foundation
+
+struct SuggestedWorkout {
+    var name: String
+    //dalam detik
+    var duration: Int
+}
---
+++ Mini2 Athens/Utilities/ExerciseTimer.swift
@@ -0,0 +1,12 @@
+//
+//  Timer.swift
+//  Mini2 Athens
+//
+//  Created by Maurice Tin on 08/06/21.
+//
+
+import Foundation
+
+struct WorkoutTimer {
+    
+}
