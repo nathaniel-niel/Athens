cf3f4581ca5926ffa1dc986790ec01a16a5831f9
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-06-10 15:55:54 +0700

    tambah lagi

---
+++ Mini2 Athens/Controllers/ExerciseProgramListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExerciseProgramListViewController.swift
+//  Mini2 Athens
+//
+//  Created by Rostadhi Akbar, M.Pd on 10/06/21.
+//
+
+import Foundation
+import UIKit
+
+class ExerciseProgramListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
