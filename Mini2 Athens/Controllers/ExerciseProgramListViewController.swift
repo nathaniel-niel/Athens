f21b04eb19f88e1345bade2728fbbdf64c692d72
LowSpecx <mauricetin.916@gmail.com>
2021-06-13 22:05:32 +0700

    Exercise Program Storyboard Re-created & connected to start program

--- Mini2 Athens/Controllers/ExerciseProgramListViewController.swift
+++ Mini2 Athens/Controllers/ExerciseProgramListViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ExerciseProgramListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     
-    
     @IBOutlet weak var exerciseProgramListTableView: UITableView!
-    @IBOutlet weak var startButton: UIButton!
+    
     
     let exercises = [
         Exercise(name: "Negative", age: 1, type: .exercise1),
@@ -29,27 +28,35 @@ class ExerciseProgramListViewController: UIViewController, UITableViewDataSource
         
         exerciseProgramListTableView.dataSource = self
         exerciseProgramListTableView.delegate = self
+        let nib = UINib(nibName: "\(ExerciseTableViewCell.self)", bundle: nil)
         
+        exerciseProgramListTableView.register(nib, forCellReuseIdentifier: "ExerciseCell")
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return exercises.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "exerciseListIdentifier") as! ExerciseProgramListTableViewCell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "exerciseListIdentifier") as! ExerciseProgramListTableViewCell
+//
+//        cell.eView.layer.cornerRadius = 30
+//        cell.eView.layer.borderWidth = 3
+//        cell.eView.layer.masksToBounds = true
+//        cell.eView.backgroundColor = exercises[indexPath.row].type?.getColor()
+//        cell.exerciseProgramImageView.image = exercises[indexPath.row].type?.getImage()
+//        cell.nameLabel.text = exercises[indexPath.row].name
+//        cell.typeLabel.text = exercises[indexPath.row].type?.rawValue
         
-        cell.eView.layer.cornerRadius = 30
-        cell.eView.layer.borderWidth = 3
-        cell.eView.layer.masksToBounds = true
-        cell.eView.backgroundColor = exercises[indexPath.row].type?.getColor()
-        cell.exerciseProgramImageView.image = exercises[indexPath.row].type?.getImage()
-        cell.nameLabel.text = exercises[indexPath.row].name
-        cell.typeLabel.text = exercises[indexPath.row].type?.rawValue
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExerciseCell") as! ExerciseTableViewCell
         
-        
+        cell.exerciseIcon.image = exercises[indexPath.row].type?.getImage()
+        cell.exerciseName.text = exercises[indexPath.row].name
+        cell.exerciseDescription.text = exercises[indexPath.row].type?.rawValue
         
         return cell
     }
+    
     @IBAction func startProgram(_ sender: UIButton){
         //     tinggal masukin identifier ke page yang maurice kerjain
 //        performSegue(withIdentifier: "", sender: self)
