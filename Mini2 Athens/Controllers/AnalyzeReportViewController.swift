d630ece724af3caef18a4ca44c93cf1e465e3836
Dkrosch <68946876+Dkrosch@users.noreply.github.com>
2021-06-15 14:30:41 +0700

    update ui

--- Mini2 Athens/Controllers/AnalyzeReportViewController.swift
+++ Mini2 Athens/Controllers/AnalyzeReportViewController.swift
@@ -21,12 +21,20 @@ class AnalyzeReportViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        reportBox.layer.cornerRadius = 30
+        reportBox.layer.cornerRadius = 20
         reportBox.layer.borderWidth = 2
-        reportBox.layer.borderColor = UIColor.black.cgColor
-        movementBox.layer.cornerRadius = 30
+        reportBox.layer.borderColor = #colorLiteral(red: 0.1647058824, green: 0.1921568627, blue: 0.1960784314, alpha: 1)
+        reportBox.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        reportBox.layer.shadowRadius = 8
+        reportBox.layer.shadowOffset = .zero
+        reportBox.layer.shadowOpacity = 0.3
+        movementBox.layer.cornerRadius = 20
         movementBox.layer.borderWidth = 2
-        movementBox.layer.borderColor = UIColor.black.cgColor
+        movementBox.layer.borderColor = #colorLiteral(red: 0.09538901597, green: 0.1101467088, blue: 0.1185845062, alpha: 1)
+        movementBox.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        movementBox.layer.shadowRadius = 8
+        movementBox.layer.shadowOffset = .zero
+        movementBox.layer.shadowOpacity = 0.3
     }
   
 }
