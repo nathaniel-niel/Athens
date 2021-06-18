5e3e61ae00b0a300cce33deb31472a4900c757aa
OidneR <michaelrandio2000@gmail.com>
2021-06-18 15:26:50 +0700

    summary Report Done

--- Mini2 Athens/Controllers/CameraView.swift
+++ Mini2 Athens/Controllers/CameraView.swift
@@ -48,6 +48,12 @@ class CameraView: UIView {
 
     
     func showPoints(_ leftPoints: [CGPoint], rightPoints: [CGPoint], color: UIColor, direction: bodyDirection) {
+        
+        guard leftPoints.count > 0 && rightPoints.count > 0 else{
+            pointsPath.removeAllPoints()
+            overlayLayer.path = pointsPath.cgPath
+            return
+        }
         pointsPath.removeAllPoints()
         var currentPoint: CGPoint!
         switch direction {
@@ -110,5 +116,73 @@ class CameraView: UIView {
         CATransaction.commit()
     }
     
+//    func showPoints2(_ leftPoints: [bodyPoint], rightPoints: [bodyPoint], direction: bodyDirection) {
+//
+//        guard leftPoints.count > 0 && rightPoints.count > 0 else{
+//            pointsPath.removeAllPoints()
+//            overlayLayer.path = pointsPath.cgPath
+//            return
+//        }
+//        pointsPath.removeAllPoints()
+//        var currentPoint: CGPoint!
+//        switch direction {
+//        case .front:
+//            // draw left side
+//            currentPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: leftPoints.first!.point)
+//            for point in leftPoints {
+//                let newPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: point.point)
+//                pointsPath.move(to: currentPoint)
+//                pointsPath.addArc(withCenter: newPoint, radius: 2.5, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+//                pointsPath.addLine(to: newPoint)
+//                currentPoint = newPoint
+//            }
+//            // draw right side
+//            currentPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: rightPoints.first!.point)
+//            for point in rightPoints {
+//                let newPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: point.point)
+//                pointsPath.move(to: currentPoint)
+//                pointsPath.addArc(withCenter: newPoint, radius: 2.5, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+//                pointsPath.addLine(to: newPoint)
+//                currentPoint = newPoint
+//            }
+//            // Connect BothSide
+//
+//            do{
+//                pointsPath.move(to: previewLayer.layerPointConverted(fromCaptureDevicePoint: leftPoints[2].point))
+//                pointsPath.addLine(to: previewLayer.layerPointConverted(fromCaptureDevicePoint: rightPoints[2].point))
+//                pointsPath.move(to: previewLayer.layerPointConverted(fromCaptureDevicePoint: leftPoints[3].point))
+//                pointsPath.addLine(to: previewLayer.layerPointConverted(fromCaptureDevicePoint: rightPoints[3].point))
+//            }
+//
+//        case .left:
+//            // draw left side
+//            currentPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: leftPoints.first!)
+//            for point in leftPoints {
+//                let newPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: point)
+//                pointsPath.move(to: currentPoint)
+//                pointsPath.addArc(withCenter: newPoint, radius: 2.5, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+//                pointsPath.addLine(to: newPoint)
+//                currentPoint = newPoint
+//            }
+//
+//        case .right:
+//            // draw right side
+//            currentPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: rightPoints.first!)
+//            for point in rightPoints {
+//                let newPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: point)
+//                pointsPath.move(to: currentPoint)
+//                pointsPath.addArc(withCenter: newPoint, radius: 2.5, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+//                pointsPath.addLine(to: newPoint)
+//                currentPoint = newPoint
+//            }
+//        }
+//        overlayLayer.fillColor = color.cgColor
+//        CATransaction.begin()
+//        CATransaction.setDisableActions(true)
+//        overlayLayer.path = pointsPath.cgPath
+//        overlayLayer.lineWidth = 5
+//        overlayLayer.strokeColor = color.cgColor
+//        CATransaction.commit()
+//    }
     
 }
